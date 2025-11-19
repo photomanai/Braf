cd642e3c305890de2b185916b26a8a6e805265e8
photomanai <photom4n.41@gmail.com>
2025-11-20 01:14:08 +0400

    Dir and LFI search sperate

--- braf.py
+++ braf.py
@@ -4,6 +4,7 @@ import requests
 import sys
 import os
 import argparse
+from urllib.parse import urljoin, urlparse
 
 def GetUserInput():
     defaultWordListPath = os.path.join(os.getcwd(), "payloads/common.txt")
@@ -11,13 +12,13 @@ def GetUserInput():
         description="bref.py - Works with url and optional parameters"
     )
 
-    parser.add_argument("-u", "--url", type=str, help="url to brute")
+    parser.add_argument("-u", "--url", type=str,required=True, help="url to brute")
 
-    parser.add_argument("-t", "--test", type=str, help="Test message", default=None)
+    # parser.add_argument("-t", "--test", type=str, help="Test message", default=None)
 
     parser.add_argument("-y", action="store_true", help="Enable aggressive mode")
     parser.add_argument(
-        "-o",
+        "-w",
         "--wordlist",
         metavar="PATH",
         default=defaultWordListPath,
@@ -36,11 +37,11 @@ def GetUserInput():
 
     return {
         "url": args.url,
-        "testMessage": args.test,
         "aggressiveMode": args.y,
         "wordListPath": args.wordlist,
         "speed": args.speed,
     }
+        # "testMessage": args.test,
 
 
 
@@ -48,8 +49,15 @@ class Force:
     def __init__(self, url, word_list_path):
         self.url = url
         self.word_list_path = word_list_path
-        print(self.url, self.word_list_path)
-    def brute(self):
+        self.status_code = [200, 201, 204, 301, 302, 307, 401, 403]
+        self.normalize_url()
+
+    def normalize_url(self):
+        parsed = urlparse(self.url)
+        if not parsed.scheme:
+            self.url = f"https://{self.url}"
+
+    def lfi_brute(self):
         with open(self.word_list_path, "r") as f:
             payloads = [line.strip() for line in f if line.strip()]
 
@@ -66,13 +74,33 @@ class Force:
             except Exception as e:
                 print(f"[!]ERROR: {e}")
 
+    def dir_brute(self):
+        with open(self.word_list_path, "r") as f:
+            payloads = [line.strip() for line in f if line.strip()]
+
+        print(f"[+] Payload lines: {len(payloads)}")
+
+        # os.path.join(os.getcwd(), "payloads/common.txt")
+        for payload in payloads:
+            try:
+                # fu = f"{self.url}/{payload}"
+                fu = urljoin(self.url, payload)
+                print(fu)
+                r = requests.get(fu, timeout=5)
+                if self.status_code in r.status_code or len(r.text) > 320:
+                    print(f"[+] VULN!: {payload}")
+                    print(f"    length: {len(r.text)} | Status: {r.status_code}")
+                else:
+                    print(f"[-] Code: {r.status_code}")
+            except Exception as e:
+                print(f"[!]ERROR: {e}")
+
 
 def main():
     try:
         userArgs = GetUserInput()
         force = Force(userArgs["url"], userArgs["wordListPath"])
-        force.brute()
-        print(userArgs)
+        force.dir_brute()
     except KeyboardInterrupt:
         print("EXIT...")
         sys.exit(1)
