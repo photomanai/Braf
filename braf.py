22792c9b923ed0cc269f9a5c5d977c0c867e4b71
photomanai <photom4n.41@gmail.com>
2025-11-25 11:58:01 +0400

    FixSome Bugs

--- braf.py
+++ braf.py
@@ -18,6 +18,7 @@ def GetUserInput():
     # parser.add_argument("-t", "--test", type=str, help="Test message", default=None)
 
     parser.add_argument("-y", action="store_true", help="Enable aggressive mode")
+    parser.add_argument("-v", action="store_true", help="Enable verbose")
     parser.add_argument(
         "-w",
         "--wordlist",
@@ -34,6 +35,7 @@ def GetUserInput():
         metavar="LEVEL",
         help="Speed level: 0‑5 (default: %(default)s)",
     )
+    parser.add_argument("-A", "--attackType", type=str, default="dir", help="Choose Your Attack Type: dir, lfi (default: dir)")
     args = parser.parse_args()
 
     return {
@@ -41,16 +43,19 @@ def GetUserInput():
         "aggressiveMode": args.y,
         "wordListPath": args.wordlist,
         "speed": args.speed,
+        "type": args.attackType,
+        "verbose": args.v
     }
         # "testMessage": args.test,
 
 
 
 class Force:
-    def __init__(self, url, word_list_path):
+    def __init__(self, url, word_list_path, userArgs):
         self.url = url
         self.word_list_path = word_list_path
         self.status_codes = [200, 201, 204, 301, 302, 307, 401, 403]
+        self.userArgs = userArgs
         self.normalize_url()
 
     def normalize_url(self):
@@ -58,34 +63,50 @@ class Force:
         if not parsed.scheme:
             self.url = f"https://{self.url}"
 
+    def check(self, url,payload):
+        fu = urljoin(self.url, payload)
+        try:
+            r = requests.get(fu, timeout=7, allow_redirects=True)
+            if self.userArgs["type"].lower() == "dir":
+                if r.status_code in self.status_codes:
+                    print(f"[+] {r.status_code} {len(r.text):>6} → {fu}")
+                    #    print(f"{'payload:':>25} {payload}")
+                elif self.userArgs["verbose"]:
+                    print(f"[-] Code: {r.status_code}")
+            elif self.userArgs["type"].lower() == "lfi":
+                if "root:" in r.text: # or len(r.text) > 320
+                    print(f"[+] VULN!: {payload}")
+                    print(f"    length: {len(r.text)} | Status: {r.status_code}")
+                elif self.userArgs["verbose"]:
+                    print(f"[-] Code: {r.status_code}")
+        except Exception as e:
+            print(f"[!]ERROR: {e}")
+
+
     def lfi_brute(self):
+        print("[+] Entered LFi type attack")
         with open(self.word_list_path, "r") as f:
             payloads = [line.strip() for line in f if line.strip()]
 
         print(f"[+] Payload lines: {len(payloads)}")
 
-        for payload in payloads:
-            try:
-                r = requests.get(self.url + payload, timeout=5)
-                if "root:" in r.text or len(r.text) > 320:
-                    print(f"[+] VULN!: {payload}")
-                    print(f"    length: {len(r.text)} | Status: {r.status_code}")
-                else:
-                    print(f"[-] Code: {r.status_code}")
-            except Exception as e:
-                print(f"[!]ERROR: {e}")
 
-    def check(self, url,payload):
-        fu = urljoin(self.url, payload)
-        try:
-            r = requests.get(fu, timeout=7, allow_redirects=True)
-            if r.status_code in self.status_codes:
-               print(f"[+] {r.status_code} {len(r.text):>6} → {fu}")
-        except:
-            pass
+        with ThreadPoolExecutor(max_workers=100) as executor:
+            executor.map(lambda p: self.check(self.url, p), payloads)
 
+        # for payload in payloads:
+        #     try:
+        #         r = requests.get(self.url + payload, timeout=5)
+        #         if "root:" in r.text: # or len(r.text) > 320
+        #             print(f"[+] VULN!: {payload}")
+        #             print(f"    length: {len(r.text)} | Status: {r.status_code}")
+        #         else:
+        #             print(f"[-] Code: {r.status_code}")
+        #     except Exception as e:
+        #         print(f"[!]ERROR: {e}")
 
     def dir_brute(self):
+        print("[+] Entered DIR type attack")
         with open(self.word_list_path, "r") as f:
             payloads = [line.strip() for line in f if line.strip()]
 
@@ -93,27 +114,18 @@ class Force:
 
         with ThreadPoolExecutor(max_workers=100) as executor:
             executor.map(lambda p: self.check(self.url, p), payloads)
-        # os.path.join(os.getcwd(), "payloads/common.txt")
-        # for payload in payloads:
-        #     try:
-        #         # fu = f"{self.url}/{payload}"
-        #         fu = urljoin(self.url, payload)
-        #         r = requests.get(fu, timeout=5)
-        #         if r.status_code in self.status_codes:
-        #             print(f"[+] {r.status_code} - {fu}")
-        #             # print(f"    length: {len(r.text)} | Status: {r.status_code}")
-        #         else:
-        #             pass
-        #             # print(f"[-] Code: {r.status_code}")
-        #     except Exception as e:
-        #         print(f"[!] ERROR: {e}")
 
 
 def main():
     try:
         userArgs = GetUserInput()
-        force = Force(userArgs["url"], userArgs["wordListPath"])
-        force.dir_brute()
+        force = Force(userArgs["url"], userArgs["wordListPath"], userArgs)
+        if userArgs["type"].lower() == "dir":
+            force.dir_brute()
+        elif userArgs["type"].lower() == "lfi":
+            force.lfi_brute()
+        else:
+            print(f"[!] ERROR: {userArgs["type"]} attack type not exist")
     except KeyboardInterrupt:
         print("EXIT...")
         sys.exit(1)
